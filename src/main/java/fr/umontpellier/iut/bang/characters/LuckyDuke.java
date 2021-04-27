53d10cebab2cce577f2fd565936b581aaa6f64cd
Tom Fondville <tom.fondville@etu.umontpellier.fr>
2021-04-27 23:29:15 +0200

    avancement de LuckyDuke

--- src/main/java/fr/umontpellier/iut/bang/characters/LuckyDuke.java
+++ src/main/java/fr/umontpellier/iut/bang/characters/LuckyDuke.java
@@ -1,9 +1,23 @@
 package fr.umontpellier.iut.bang.characters;
 
+import fr.umontpellier.iut.bang.Player;
+import fr.umontpellier.iut.bang.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuckyDuke extends BangCharacter {
 
     public LuckyDuke() {
         super("Lucky Duke", 4);
+    }
+
+    @Override
+    public Card randomDraw(Player player) {
+        List<Card> drawnCards = new ArrayList<>();
+        drawnCards.add(player.drawCard());
+        drawnCards.add(player.drawCard());
+        return player.chooseCard("Choisissez la carte à dégainer",drawnCards,false,false);
     }
 
 }
