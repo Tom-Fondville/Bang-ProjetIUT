b417bbcfbce0afe183bede5f7432f1ffcb7af152
nancy <nancy.siong@etu.umontpellier.fr>
2021-04-29 20:49:18 +0200

    Implementation de Indians

--- src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
@@ -1,9 +1,34 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.stream.Collectors;
+
 public class Indians extends OrangeCard {
 
     public Indians(int value, CardSuit suit) {
         super("Indians!", value, suit);
     }
 
+    @Override
+    public void playedBy(Player player) {
+
+        for (Player p : player.getOtherPlayers()) {
+            Card b = p.chooseCard(
+                    "Défaussez-vous d'une carte Bang!",
+                    p.getHand().stream().filter(m -> m.getName().equals("Bang!")).collect(Collectors.toList()),
+                    false,
+                    true);
+
+            if (b != null) {
+                p.removeFromHand(b);
+                p.discard(b);
+            }
+            else {
+                p.decrementHealth(1, null);
+            }
+        }
+    }
 }
+
+
